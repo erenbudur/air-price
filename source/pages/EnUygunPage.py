06645979451f52421cfa106dc957009bd2682e43
Eren Budur <erenbudur33@gmail.com>
2022-08-04 00:36:28 +0300

    Added object to hold offer and flight info (Incomplete)

--- source/pages/EnUygunPage.py
+++ source/pages/EnUygunPage.py
@@ -1,13 +1,36 @@
 
+from lib2to3.pgen2 import driver
 from bs4 import Tag
 import click
+from numpy import double
 from selenium import webdriver
+import selenium
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import locators
 
+class Offer:
+    def __init__(self,driver :webdriver.Chrome,offer) -> None:
+        flights = offer.find_elements(By.CLASS_NAME,locators.FLIGHT)
+        self.departure = Flight(driver,flights[0])
+        self.arrival = Flight(driver,flights[1])
+        self.price = float(offer.find_element(By.CLASS_NAME,locators.PRICE).text)
+        self.currency = offer.find_element(By.CLASS_NAME,locators.CURRENCY).text
+
+class Flight:
+    def __init__(self,driver:webdriver.Chrome,flight) -> None:
+        
+        self.element = flight
+        self.airlines = []
+        companies = flight.find_elements(By.CLASS_NAME,locators.SUMMARY_AIRLINE)
+        for company in companies:
+            self.airlines.append(company.text)
+        
+        # self.departureTime =
+        # self.arrivalTime =
+        # self.duration =
 
 """
 Dates must be in the format of dd.mm.yyyy
@@ -21,11 +44,17 @@ def searchEnUygun(departureDate,returnDate,numPeople,tripType):
         wait = WebDriverWait(driver,10)
         #international
         driver.get(start_url + departureDate+ "&donus=" + returnDate + "&yetiskin=" + numPeople + "&sinif=ekonomi&save=1&geotrip=international&1trip=" + tripType)
+        driver.maximize_window()
         time.sleep(15)
-        offerlist = driver.find_elements(By.XPATH,locators.FLIGHT_LIST)
+        driver.find_element(By.XPATH,locators.TWO_TRANSFER).click()
+        time.sleep(5)
+        offerlist = driver.find_elements(By.XPATH,locators.OFFER_LIST)
+        
 
-        for trips in offerlist:
-            trips.get
+
+        for offers in offerlist:
+            
+            offer = Offer(driver,offers)
 
 
-#searchEnUygun("26.08.2022","21.12.2022","1","international")
+searchEnUygun("26.08.2022","21.12.2022","1","international")
--- source/pages/locators.py
+++ source/pages/locators.py
@@ -1,1 +1,8 @@
-FLIGHT_LIST= '//*[@id="SearchRoot"]//div[contains(@class ,"flightItemContainer" )]'
+OFFER_LIST= '//*[@id="SearchRoot"]//div[contains(@class ,"flightItemContainer" )]'
+TWO_TRANSFER ='//span[text()="2+ aktarma "]'
+FLIGHT = 'flight-summary'
+AIRLINE = '//*[@class="summary-airline"]'
+PRICE = 'money-int'
+CURRENCY = 'money-currency'
+SUMMARY_AIRLINE ='summary-airline'
+# (//*[@id="SearchRoot"]//div[contains(@class ,"flightItemContainer" )])[4]//*[@class="summary-airline"]
